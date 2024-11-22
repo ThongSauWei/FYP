5f8a487d586db4b299848216a5d21487d7d94841
sauweiiiii~ <114349729+ThongSauWei@users.noreply.github.com>
2024-11-23 01:53:22 +0800

    2

--- app/src/main/java/com/example/fyp/SearchPost.kt
+++ app/src/main/java/com/example/fyp/SearchPost.kt
@@ -1,4 +1,18 @@
 package com.example.fyp
 
-class SearchPost {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SearchPost : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
 }
